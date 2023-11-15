f8c262a2ccb7dba89378158ab2a33e76f0789acd
Mazahir <Mazahir-Hussain-malik>
2023-11-15 18:00:57 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+console.log("hello javascript");
+alert("hello javascript");
+document.write("hello javascript");
+
+// this are the keywords that declare vaibales
+// var
+// let
+// const
+var num1 = 10;
+console.log("the number is ", num1);
+
+let num2 = 20;
+console.log("the let varibale value is ", num2);
+
+const num3 = 30;
+console.log("const value is ", num3);
+
+// javascript data types
+// 1 - Number
+// 2 - String
+// 3 - Boolean
+// 4 - undefined
+// 5 - null
+// 6 - objects
+// 7 - arrays
+// 8 - symbol
+// 9 - bigint
+
+let name = "triangle";
+console.log("office name is ", name);
+
+console.log("3 > 5 = ", 3 > 5); //false
+
+let a;
+console.log("a is = ", a);
+
+let firstname = null;
+console.log("firstname is =  ", firstname);
+
+// objects
+// key value data store
+//   num1 : 20
+
+const person = {
+  firstname: "ali",
+  lastname: "muhammad",
+  phone: 12,
+  email: "ali@gmail.com",
+};
+
+console.log("object is here ", person);
+console.log("person name is", person.firstname);
+
+// arrays
+
+let fruits = ["apple", "banana", "mango", "watermellon", "pineapple"];
+
+console.log("fruits are  : ", fruits);
+console.log("first item in array is : ", fruits[0]);
+console.log("third item in array is : ", fruits[2]);
+
+//
